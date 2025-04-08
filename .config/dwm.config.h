404afbb7fd15c48f586f8b577bf5f850cfb6e9cc
FelipeIzolan <80170121+FelipeIzolan@users.noreply.github.com>
2025-04-08 02:17:47 -0300

    Hello, World!

---
+++ .config/dwm.config.h
@@ -0,0 +1,124 @@
+/* See LICENSE file for copyright and license details. */
+
+/* appearance */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+
+static const unsigned int gappih    = 4;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 4;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 4;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 4;       /* vert outer gap between windows and screen edge */
+static                int smartgaps = 0; 
+
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+
+static const char *fonts[]          = { "CaskaydiaCove Nerd Font:size=12" };
+static const char *colors[][3]      = {
+	/*               fg         bg         border   */
+	[SchemeNorm] = { "#ebdbb2", "#282828", "#3c3836" },
+	[SchemeSel]  = { "#fbf1c7", "#d65d0e", "#d65d0e"  },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
+
+static const Rule rules[] = {
+	/* xprop(1):
+	 *	WM_CLASS(STRING) = instance, class
+	 *	WM_NAME(STRING) = title
+	 */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+};
+
+/* layout(s) */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
+static const Layout layouts[] = {
+ 	/* symbol     arrange function */
+	{ "[@]",      spiral },
+	{ "[]=",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+ 	{ "[M]",      monocle },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY,TAG) \
+	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* commands */
+static const char *termcmd[]  = { "st", NULL };
+static const char *launchercmd[]  = { "rofi", "-show", "drun", NULL };
+
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
+
+static const Key keys[] = {
+	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_space,  spawn,          {.v = launchercmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_Right,  focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Left,   focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_Up,     setlayout,      {.i = -1 } },
+	{ MODKEY,                       XK_Down,   setlayout,      {.i = +1 } },	
+	TAGKEYS(                        XK_1,                      0)
+	TAGKEYS(                        XK_2,                      1)
+	TAGKEYS(                        XK_3,                      2)
+	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_5,                      4)
+	TAGKEYS(                        XK_6,                      5)
+	TAGKEYS(                        XK_7,                      6)
+	TAGKEYS(                        XK_8,                      7)
+	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+	/* click                event mask      button          function        argument */
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkTagBar,            0,              Button1,        view,           {0} },
+	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
---
+++ install.sh
@@ -0,0 +1,81 @@
+sudo xbps-install -S xorg-minimal\
+                     xorg-fonts\
+                     setxkbmap\
+                     xwallpaper\
+                     pipewire\
+                     wireplumber\
+                     dbus\
+                     turnstile\
+                     rofi
+
+sudo xbps-install git\
+                  curl\
+		              patch\
+		              xdg-user-dirs
+
+sudo xbps-install base-devel\
+                  libX11-devel\
+                  libXft-devel\
+                  libXinerama-devel
+
+#####################################################################
+
+dwmp=(
+  "https://dwm.suckless.org/patches/vanitygaps/dwm-cfacts-vanitygaps-6.4_combo.diff"
+  "https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff"
+  "https://dwm.suckless.org/patches/underlinetags/dwm-underlinetags-6.2.diff"
+  "https://dwm.suckless.org/patches/notitle/dwm-notitle-20210715-138b405.diff"
+)
+
+stp=(
+  "https://st.suckless.org/patches/scrollback/st-scrollback-0.9.2.diff"
+  "https://st.suckless.org/patches/scrollback/st-scrollback-mouse-0.9.2.diff"
+  "https://st.suckless.org/patches/font2/st-font2-0.8.5.diff"
+  "https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.5.diff"
+)
+
+git clone https://git.suckless.org/dwm ./source/dwm
+git clone https://git.suckless.org/st ./source/st
+
+for value in ${dwmp[@]}; do
+  curl -q ${value} | patch -d ./source/dwm -i -
+done
+
+for value in ${stp[@]}; do
+  curl -q ${value} | patch -d ./source/st -i -
+done
+
+cp -f ./.config/dwm.config.h ./source/dwm/config.h
+cp -f ./.config/st.config.h ./source/st/config.h
+
+sudo make clean install -C ./source/dwm
+sudo make clean install -C ./source/st
+
+#####################################################################
+
+sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/turnstiled /var/service
+
+#####################################################################
+
+cp -f ./wallpaper.png ~/wallpaper.png
+cp -f ./.xinitrc ~/.xinitrc
+cp -rf ./.config/rofi ~/.config/rofi
+
+sudo cp -f ./CaskaydiaCoveNerdFont-Regular.tff /usr/share/fonts
+
+#####################################################################
+
+xdg-user-dirs-update
+
+sudo xbps-remove -R git\
+                    curl\
+		                patch\
+		                xdg-user-dirs\
+		                base-devel\
+		                libX11-devel\
+		                libXft-devel\
+		                libXinerama-devel\
+		                libXext-devel
+
+sudo xbps-remove -Oo
